509174c8371c256d0729c5911de08d70bd8d70e0
Alistair Millard <millardaly@gmail.com>
2018-05-14 22:06:11 +0100

    changed directory names to better match their roles

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const routes = require('express').Router();
+const all = require('./all');
 const models = require('./models');
 const cars = require('./cars');
 
+routes.use('/', all);
 routes.use('/models', models);
 routes.use('/cars', cars);
 
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')();
-const routes = require('./routes');
+const api = require('./api');
 
-app.use('/', routes);
+app.use('/api', api);
 
 app.listen(3000, () => {
   console.log('App listening on port http://localhost:3000');  
